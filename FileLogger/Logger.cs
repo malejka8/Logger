18cb8f17c874835a9058a9e75161d347d1954170
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-01-29 18:39:48 +0100

    Nowy projekt FileLogger

---
+++ FileLogger/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLogger
+{
+    public class Logger
+    {
+        private readonly string fileName;
+        //const string fileName2 = "stała";
+
+        public Logger()
+        {
+
+        }
+        public Logger(string fileName)
+        {
+            this.fileName = fileName;
+        }
+    }
+}
--- Logger/Program.cs
+++ Logger/Program.cs
@@ -10,7 +10,22 @@ namespace Logger
     {
         static void Main(string[] args)
         {
-
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine(args[i]);
+            //}
+            List<string> temp = new List<string>();
+            temp.AddRange(args);
+            //foreach (var item in args)
+            //{
+            //    temp.Add(item);
+            //}
+            while (temp.Count > 0)
+            {
+                Console.WriteLine(temp[0]);
+                temp.Remove(temp[0]);
+            }
+            Console.Read();
         }
     }
 }
