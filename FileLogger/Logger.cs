d7c3610e43648c3f2b118be3359480ce6e00a249
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-01-29 19:58:30 +0100

    Kolejne zmiany

--- FileLogger/Logger.cs
+++ FileLogger/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,44 @@ namespace FileLogger
     {
         private readonly string fileName;
         //const string fileName2 = "stała";
+        private static string directory;
 
         public Logger()
         {
+            setDirectory();
+        }        
 
-        }
         public Logger(string fileName)
         {
             this.fileName = fileName;
+            setDirectory();
+        }
+        public void Log(string text)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            //File.AppendAllLines(Path.Combine(directory, fileName), new string[] { text });
+            text = $"{Environment.NewLine} Data: {DateTime.Now.ToShortDateString()} Informacja: {text}";
+            File.AppendAllText(Path.Combine(directory, fileName), text);
+        }
+        private void setDirectory()
+        {
+            try
+            {               
+                directory = System.Configuration.ConfigurationManager.AppSettings["dir"];
+                //var file = System.Configuration.ConfigurationManager.OpenExeConfiguration("~/FileLogger.dll.config");
+                //directory = file.AppSettings["dir"].ToString();
+            }
+            catch (FileNotFoundException fnfe)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
--- Logger/Program.cs
+++ Logger/Program.cs
@@ -10,22 +10,14 @@ namespace Logger
     {
         static void Main(string[] args)
         {
-            //for (int i = 0; i < args.Length; i++)
-            //{
-            //    Console.WriteLine(args[i]);
-            //}
-            List<string> temp = new List<string>();
-            temp.AddRange(args);
-            //foreach (var item in args)
-            //{
-            //    temp.Add(item);
-            //}
-            while (temp.Count > 0)
+            FileLogger.Logger logger = new FileLogger.Logger("Info.txt");
+            while (true)
             {
-                Console.WriteLine(temp[0]);
-                temp.Remove(temp[0]);
+                Console.WriteLine("Wpisz coś");
+                var text = Console.ReadLine();                
+                logger.Log(text);
+                //Console.Read();
             }
-            Console.Read();
         }
     }
 }
